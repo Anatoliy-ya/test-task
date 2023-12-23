c50fcd5f6cde5d7114a5cccbfbdef21da656d401
Anatoliy-ya <salamons777@gmail.com>
2023-12-23 19:58:40 +0300

    Chabge App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from '@/components/mainPage/mainPage';
 import StepOne from '@/components/steps/stepOne';
 import StepTwo from '@/components/steps/stepTwo';
